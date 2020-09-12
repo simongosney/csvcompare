60366b1ffd0d9a7cecfa77c4be56fb01985c21a5
Simon Gosney <simon@samandsimon.com>
2020-09-12 09:53:27 +0100

    Updates to script and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # csvcompare
  
+filecompare.ps1 takes two parameters:
+
+    -inputFilesDirectory  [path to files that were provided]
+    -outputFilesDirectory [path to exported files]
+
+Script is predicated on equivalent files in the two directories having the same filename and having .csv extension.
+
+Analysis is output by default to new directory in the location that script is run from.
--- filecompare.ps1
+++ filecompare.ps1
@@ -1,27 +1,45 @@
-#this post probably has the answer to the problem:
-# https://stackoverflow.com/questions/18259861/compare-object-not-working-if-i-dont-list-the-properties/18346380
+Param(
+    [Parameter(Mandatory=$true)][string] $inputFilesDirectory,
+    [Parameter(Mandatory=$true)][string] $outputFilesDirectory,
+    [string] $compareFileDirectory = ".\"
+)
 
-$inputFile   = Import-Csv -Path .\input.csv
-$outputFile  = Import-Csv -Path .\output.csv
-#Compare-Object -ReferenceObject $input -DifferenceObject $output -Property 'Unique ID' -PassThru  | Format-Table -AutoSize
-Compare-Object -ReferenceObject $inputFile -DifferenceObject $outputFile -PassThru  
-        -Property
+# create compare output directory for this execution
+$datePortion = Get-Date -Format "yyyyMMdd_HHmmss"
+$compareOutputDirectory = Join-Path $compareFileDirectory $datePortion
 
-#| Format-Table -AutoSize 
+# create output directory
+New-Item $compareOutputDirectory -ItemType directory
 
+# Loop through files in input directory and process
+$inputFilesList = Get-ChildItem -Path $inputFilesDirectory -Filter *.csv
+foreach ($file in $inputFilesList)
+{
+    # Load input file into object
+    $inputCollection = Import-Csv -Path $file.FullName
 
+    # get equivelent output file
+    $outputFile = Join-Path -Path $outputFilesDirectory -ChildPath $file.name
+    $outputFile
+    $outputFileExists = Test-Path $outputFile
+    if ($outputFileExists -eq $false)
+    {
+        Write-Host("No matching output file for $file.FullName")
+            break
+    }
 
-# The original collection.
-$inputCollection = Import-Csv -Path .\input.csv
-$exportCollection = Import-Csv -Path .\output.csv 
+    # load equivalent output file into object
+    $exportCollection = Import-Csv -Path $outputFile
 
+    # derive compare file name
+    $compareFile = Join-Path $compareOutputDirectory $file.name 
+    $compareFile
 
+    # Get the array of all property names to compare from the original collection.
+    $propsToCompare = $inputCollection[0].psobject.properties.name
 
-# PSv3+: Get the array of all property names to compare 
-#        from the original collection.
-$propsToCompare = $inputCollection[0].psobject.properties.name
-
-# Compare the 2 collections by all property values.
-# -PassThru means that any input object in which a difference is found
-# is passed through as-is.
-Compare-Object -ReferenceObject $inputCollection -DifferenceObject $exportCollection -Property $propsToCompare -PassThru
+    # Compare the 2 collections by all property values.
+    # -PassThru means that any input object in which a difference is found
+    # is passed through as-is.
+    Compare-Object -ReferenceObject $inputCollection -DifferenceObject $exportCollection -Property $propsToCompare -PassThru | Format-Table -AutoSize | Out-File -FilePath $compareFile 
+}
