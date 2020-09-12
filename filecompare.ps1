d305410fa287f69210a69646401fdd91216f7c45
Simon Gosney <simon@samandsimon.com>
2020-09-12 08:06:09 +0100

    Working script

--- filecompare.ps1
+++ filecompare.ps1
@@ -1,4 +1,6 @@
 $inputFile   = Import-Csv -Path .\input.csv
 $outputFile  = Import-Csv -Path .\output.csv
 #Compare-Object -ReferenceObject $input -DifferenceObject $output -Property 'Unique ID' -PassThru -IncludeEqual | Format-Table -AutoSize
-Compare-Object -ReferenceObject $input -DifferenceObject $output -PassThru -IncludeEqual | Format-Table -AutoSize
+Compare-Object -ReferenceObject $inputFile -DifferenceObject $outputFile -PassThru  | Format-Table -AutoSize
+
+
